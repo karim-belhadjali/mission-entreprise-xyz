4320eb2622d3420cae81cc6fcc75bd38fc24e326
mkboughammoura <mkboughammoura@vermeg.com>
2022-07-16 17:22:18 +0100

    add comment and postComment entities

---
+++ src/main/java/com/example/xyz/entity/Post.java
@@ -0,0 +1,23 @@
+package com.example.xyz.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private int nbrOfStars;
+    private int nbrOfViews;
+
+    @OneToMany(mappedBy="post")
+    List<PostComment> comments;
+
+}
---
+++ src/main/java/com/example/xyz/entity/PostComment.java
@@ -0,0 +1,23 @@
+package com.example.xyz.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class PostComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name="post_id", nullable=false)
+    private Post post;
+
+    @OneToOne
+    @JoinColumn(name="postComment_id", nullable=false)
+    private PostComment postComment;
+}
