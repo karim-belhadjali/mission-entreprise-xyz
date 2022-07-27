fcc6e320c0bd7c81ad0e94f6d5f5d6e5b1480b3b
Karim BEL HADJ ALI <karim.belhadjali1@esprit.tn>
2022-07-27 10:34:20 +0100

    Added the email package

---
+++ src/main/java/com/example/xyz/messagerie/controller/SimpleEmailExampleController.java
@@ -0,0 +1,37 @@
+package com.example.xyz.messagerie.controller;
+
+
+import com.example.xyz.messagerie.utils.MyConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SimpleEmailExampleController {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @ResponseBody
+    @RequestMapping("/sendSimpleEmail")
+    public String sendSimpleEmail() {
+
+        // Create a Simple MailMessage.
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(MyConstants.FRIEND_EMAIL);
+        message.setSubject("Test Simple Email");
+        message.setText("Hello, Im testing Simple Email");
+        message.setCc("karimhba97@gmail.com");
+
+
+        // Send Message!
+        this.emailSender.send(message);
+
+        return "Email Sent!";
+    }
+
+}
