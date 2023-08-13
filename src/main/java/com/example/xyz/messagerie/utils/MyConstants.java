140499bec8dc82acc72412b9eb1ac9c6363f978f
karim-belhadjali <62988844+karim-belhadjali@users.noreply.github.com>
2023-08-13 22:18:31 +0200

    Update MyConstants.java

--- src/main/java/com/example/xyz/messagerie/utils/MyConstants.java
+++ src/main/java/com/example/xyz/messagerie/utils/MyConstants.java
@@ -3,10 +3,10 @@ package com.example.xyz.messagerie.utils;
 public class MyConstants {
 
     // Replace with your email here:
-    public static final String MY_EMAIL = "karim.belhadjali1@esprit.tn";
+    public static final String MY_EMAIL = "";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "gfzbodzkzvzgeyyt";
+    public static final String MY_PASSWORD = "";
 
     // And receiver!
     public static final String FRIEND_EMAIL = "karim.belhadjali@esprit.tn";
